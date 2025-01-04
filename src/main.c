dcd8156452d0555308b03402016e8437a26dd0e6
HUMAM970 <humam.102001@gmail.com>
2025-01-04 21:47:13 +0300

    added basic user input

--- src/main.c
+++ src/main.c
@@ -2,6 +2,7 @@
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/types.h>
 
 #define SCREEN_WIDTH GetScreenWidth()
 #define SCREEN_HEIGHT GetScreenHeight()
@@ -10,33 +11,33 @@
 #define MID_X (GetScreenWidth() / 2.0) - (WINDOW_WIDTH / 2.0)
 #define MID_Y (GetScreenHeight() / 2.0) - (WINDOW_HEIGHT / 2.0)
 #define FPS 60
-#define MAX_OBJECTS 100
+#define MAX_ENTITIES 100
 
-Vector2 POSITIONS[MAX_OBJECTS];
-Vector2 VELOCITIES[MAX_OBJECTS];
-bool VALID_OBJ[MAX_OBJECTS];
+Vector2 POSITIONS[MAX_ENTITIES];
+Vector2 VELOCITIES[MAX_ENTITIES];
+bool VALID_ENTITIES[MAX_ENTITIES];
 int objs_count = 0;
 
 typedef int Enitiy_Id;
-
 typedef enum { PLAYER, ENEMY } Enitiy;
+
 typedef struct {
-    Enitiy type;
+    Enitiy enitiy;
     Enitiy_Id id;
 } Object;
 
 void init_objects() {
-    for (int i = 0; i < MAX_OBJECTS; i++) {
+    for (int i = 0; i < MAX_ENTITIES; i++) {
         POSITIONS[i] = (Vector2){0.0f, 0.0f};
         VELOCITIES[i] = (Vector2){0.0f, 0.0f};
-        VALID_OBJ[i] = false;
+        VALID_ENTITIES[i] = false;
     }
 }
 
 Enitiy_Id create_entity_id() {
     Enitiy_Id id = -1;
-    for (Enitiy_Id i = 0; i < MAX_OBJECTS; i++) {
-        if (!VALID_OBJ[i]) {
+    for (Enitiy_Id i = 0; i < MAX_ENTITIES; i++) {
+        if (!VALID_ENTITIES[i]) {
             id = i;
             break;
         }
@@ -50,11 +51,11 @@ Object create_obj(Enitiy enitiy, Vector2 pos, Vector2 vel) {
         printf("Faild To Create Object");
         exit(1);
     }
-    Object obj = (Object){.type = enitiy, .id = id};
+    Object obj = (Object){.enitiy = enitiy, .id = id};
 
     POSITIONS[id] = pos;
     VELOCITIES[id] = vel;
-    VALID_OBJ[id] = true;
+    VALID_ENTITIES[id] = true;
     objs_count++;
 
     return obj;
@@ -65,27 +66,54 @@ void drop_obj(Object obj) {
     POSITIONS[obj.id].y = 0.0f;
     VELOCITIES[obj.id].x = 0.0f;
     VELOCITIES[obj.id].y = 0.0f;
-    VALID_OBJ[obj.id] = false;
+    VALID_ENTITIES[obj.id] = false;
     objs_count--;
 }
 
-int main() {
+void load_obj_2d(Object obj, Texture2D texture) {
+    DrawTextureEx(texture, POSITIONS[obj.id], 0.0f, 2.5f, WHITE);
+}
+
+void init_game() {
     InitWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "space invaders");
     SetWindowPosition(MID_X, MID_Y);
     SetTargetFPS(FPS);
 
     init_objects();
-    Object player_one =
-        create_obj(PLAYER, (Vector2){MID_X, MID_Y}, (Vector2){0, 0});
+}
+
+int main() {
+    init_game();
+    Object player_one = create_obj(
+        PLAYER, (Vector2){GetScreenWidth() * 0.5, GetScreenHeight() * 0.9},
+        (Vector2){100, 100});
+    Image player_img = LoadImage("./assets/player.png");
+    Texture2D player_texture = LoadTextureFromImage(player_img);
+    UnloadImage(player_img);
 
     while (!WindowShouldClose()) {
+        Vector2 direction = {0, 0};
+
+        if (IsKeyDown(KEY_H))
+            direction.x -= 1;
+        if (IsKeyDown(KEY_L))
+            direction.x += 1;
+        if (IsKeyDown(KEY_J))
+            direction.y += 1;
+        if (IsKeyDown(KEY_K))
+            direction.y -= 1;
+
+        POSITIONS[player_one.id].x +=
+            direction.x * VELOCITIES[player_one.id].x * GetFrameTime();
+        POSITIONS[player_one.id].y +=
+            direction.y * VELOCITIES[player_one.id].y * GetFrameTime();
+
         BeginDrawing();
         ClearBackground(BLACK);
-        DrawRectangle(POSITIONS[player_one.id].x, POSITIONS[player_one.id].y,
-                      50, 50, BLUE);
+        load_obj_2d(player_one, player_texture);
         EndDrawing();
     }
-
     CloseWindow();
+
     return 0;
 }
